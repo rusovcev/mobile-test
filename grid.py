96329ada5ce89bb977ba839a189900b908804625
tbb <rusovcev@gmail.com>
2020-07-02 15:41:52 +0200

    alpha design

--- grid.py
+++ grid.py
@@ -2,53 +2,91 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-from kivy.graphics import Color, Rectangle
+from kivy.graphics import Color, Rectangle, RoundedRectangle
 from kivy.utils import get_color_from_hex
 
-class Grid(GridLayout):
-    # rows = 1
-    cols = 2
+class GridAll(GridLayout):
+    # cols = 3
 
     def __init__(self, **kwargs):
-        # print(kwargs)
+        # super(Grid, self).__init__(**kwargs)
+        # super(Grid, self).__init__()
+        super().__init__()
 
-        super(Grid, self).__init__()
-        # with self.canvas.before:
-        #     Color(rgb=(get_color_from_hex("#555555")))
-        #     Rectangle(size=self.size, pos=self.pos)
+        self.cols = kwargs.get("cols", 1)
+        self.rows = kwargs.get("rows", 1)
 
-        leftWidget = FloatLayout()
+        with self.canvas.before:
+            Color(rgb=(get_color_from_hex(kwargs.get("canvasColor", "#ffffff"))))
+            # self.rectangle = Rectangle(size=self.size, pos=self.pos)
+            self.rectangle = RoundedRectangle(size=self.size, pos=self.pos, radius=[10,])
+        self.bind(pos=self.update_rectangle, size=self.update_rectangle)
+
+        widget = FloatLayout()
 
         leftImage = Image(
             source="images/icons/" + kwargs.get("left_image_souce", "bitcoin.png"), 
-            size_hint=(.45, 1),
-            pos_hint={"top": 1, "left": .1})
+            size_hint=(.6, .6),
+            pos_hint={"top": .8, "left": 1})
+        widget.add_widget(leftImage)
+        self.add_widget(widget)
 
-        leftLabel = Label(
-            text=kwargs.get("left_label_text", "left text"), 
-            size_hint=(1, .5),
-            pos_hint={"top": 1, "left": .5},
+        widget = FloatLayout()
+
+        topLabel = Label(
+            text=kwargs.get("left_label_text", "top"), 
+            size_hint=(1, .2),
+            pos_hint={"top": .9, "left": 1},
+            color=get_color_from_hex("#0f0f0fff"))
+        midLabel = Label(
+            text=kwargs.get("left_label_text", "middle"), 
+            size_hint=(1, .2),
+            pos_hint={"top": .6, "left": 1},
+            color=get_color_from_hex("#0f0f0fff"))
+        bottomLabel = Label(
+            text=kwargs.get("left_label_text", "bottom"), 
+            size_hint=(1, .2),
+            pos_hint={"top": .3, "left": .5},
             color=get_color_from_hex("#0f0f0fff"))
 
-        leftWidget.add_widget(leftImage)
-        leftWidget.add_widget(leftLabel)
+        widget.add_widget(topLabel)
+        widget.add_widget(midLabel)
+        widget.add_widget(bottomLabel)
+        self.add_widget(widget)
 
-        rightWidget = FloatLayout()
+        widget = FloatLayout()
 
-        rightImage = Image(
-            source="images/icons/" + kwargs.get("right_image_source", "phone.png"), 
-            size_hint=(.4, 1), 
-            pos_hint={"top": 1, "right": 1})
+        label = Label(
+            text=kwargs.get("left_label_text", "action"), 
+            size_hint=(1, .2),
+            pos_hint={"top": .9, "left": 1},
+            color=get_color_from_hex("#0f0f0fff"))
+        # widget.add_widget(label)
+        self.add_widget(label)
 
-        rightLabel = Label(
-            text=kwargs.get("right_label_text", "right text"), 
-            size_hint=(1, .5),
-            pos_hint={"top": 1, "right": .5},
-            color=get_color_from_hex("#ff0000ff")
-        )
 
-        rightWidget.add_widget(rightImage)
-        rightWidget.add_widget(rightLabel)
 
-        self.add_widget(leftWidget)
-        self.add_widget(rightWidget)
+        # rightWidget = FloatLayout()
+
+        # rightImage = Image(
+        #     source="images/icons/" + kwargs.get("right_image_source", "phone.png"), 
+        #     size_hint=(.4, 1), 
+        #     pos_hint={"top": 1, "right": 1})
+
+        # rightLabel = Label(
+        #     text=kwargs.get("right_label_text", "right text"), 
+        #     size_hint=(1, .5),
+        #     pos_hint={"top": 1, "right": .5},
+        #     color=get_color_from_hex("#ff0000ff")
+        # )
+
+        # rightWidget.add_widget(rightImage)
+        # rightWidget.add_widget(rightLabel)
+
+        # self.add_widget(leftWidget)
+        # self.add_widget(rightWidget)
+    
+    def update_rectangle(self, *args):
+        self.rectangle.pos = self.pos
+        self.rectangle.size = self.size
+        self.rectangle.radius = [20,]
--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.uix.button import ButtonBehavior, Button
+from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.utils import get_color_from_hex
-from functools import partial
-from grid import Grid
-import requests
-import json
-import os
+from grid import GridAll
+
+# class TabScreen(Screen):
+#     pass
 
 class HomeScreen(Screen):
-    pass
-
-class TabScreen(Screen):
-    pass
-
-class SettingsScreen(Screen):
     pass
 
 class ImageButton(ButtonBehavior, Image):
@@ -28,101 +16,52 @@ class ImageButton(ButtonBehavior, Image):
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
-    favourites = []
 
     def build(self):
         return GUI
 
-    def change_screen(self, screen_name):
-        screen_manager = self.root.ids["screen_manager"]
-        screen_manager.current = screen_name
-
     def on_start(self):
-        if os.path.exists("favorites.json"):
-            with open("favorites.json", "r") as favFile:
-                self.favourites = json.load(favFile)
-        # self.test()
-        # self.get_coinmarketcap()
-        self.test2()
+        self.test()
+        pass
     
     def on_pause(self):
         pass
 
     def on_resume(self):
-        self.textpopup(title='Exit', text='Are you sure?')
+        # self.textpopup(title='Exit', text='Are you sure?')
         pass
 
     def on_stop(self):
         pass
 
     def test(self):
-        #  grid_row = self.root.ids['home_screen'].ids['grid_row']
-        grid_row = self.root.ids['tab_screen'].ids['grid_row']
-        for row in range(0, 8):
-            r = Grid(left_label_text="foo", right_label_text="bar")
-            grid_row.add_widget(r)
-        test_label = self.root.ids['home_screen'].ids['home_screen_top_label']
-        test_label.text = "TEST!"
-
-    def test2(self):
-        grid_row = self.root.ids['tab_screen'].ids['grid_main']
+        grid_row = self.root.ids['home_screen'].ids['grid_all']
         for row in range(10):
-            leftImage = Image(
-                source="images/icons/bitcoin.png", 
-                size_hint=(.45, 1),
-                pos_hint={"top": 1, "left": .1})
+            grid = GridAll(shit="happens", cols=3)
+            grid_row.add_widget(grid)
+            pass
 
-            leftLabel = Label(
-                text="left text", 
-                size_hint=(1, .5),
-                pos_hint={"top": 1, "left": .5},
-                color=get_color_from_hex("#0f0f0fff"))
+    # def test(self):
+    #     grid_row = self.root.ids['tab_screen'].ids['grid_main']
+    #     for row in range(10):
+    #         leftImage = Image(
+    #             source="images/icons/bitcoin.png", 
+    #             size_hint=(.45, 1),
+    #             pos_hint={"top": 1, "left": .1})
+
+    #         leftLabel = Label(
+    #             color=(0,0,0,1),
+    #             text="left text", 
+    #             size_hint=(1, .5),
+    #             pos_hint={"top": 1, "left": .5}
+    #             )
             
-            favBtn = ImageButton(
-                source="images/icons/001-star.png",
-                size=(10, 10),
-                on_release=partial(self.set_favourite, "fav {}".format(row)))
+    #         # favBtn = ImageButton(
+    #         #     # on_release=partial(self.set_favourite, "fav {}".format(row)),
+    #         #     source="images/icons/001-star.png")
 
-            grid_row.add_widget(leftImage)
-            grid_row.add_widget(leftLabel)
-            grid_row.add_widget(favBtn)
-    
-    def set_favourite(self, favId, widget):
-        print(favId)
-
-    def get_coinmarketcap(self):
-        apiUrl = "https://sandbox-api.coinmarketcap.com/v1/"
-        service = "cryptocurrency/listings/latest"
-        params = {"start": '1', "limit": "10", "convert": "USD"}
-        headers = {"Accepts": "application/json", "X-CMC_PRO_API_KEY": "9620bdd8-06ca-4b7d-9847-c0c68b5f36a7",}
-
-        req = requests.get(apiUrl + service, params=params, headers=headers)
-        if req.status_code == 200:
-            # print(req.text)
-            res = json.loads(req.text)
-            test_label = self.root.ids['home_screen'].ids['home_screen_top_label']
-            test_label.text = res['status']['timestamp']
-            grid_row = self.root.ids['home_screen'].ids['grid_row']
-            scroll_view = self.root.ids['home_screen'].ids['scroll_view']
-            for item in res['data']:
-                row = Grid(left_label_text=item['name'], right_label_text=str(item['quote']['USD']['price']))
-                grid_row.add_widget(row)
-
-    def textpopup(self, title='', text=''):
-        """Open the pop-up with the name.
-
-        :param title: title of the pop-up to open
-        :type title: str
-        :param text: main text of the pop-up to open
-        :type text: str
-        :rtype: None
-        """
-        box = BoxLayout(orientation='vertical')
-        box.add_widget(Label(text=text))
-        mybutton = Button(text='OK', size_hint=(1, 0.25))
-        box.add_widget(mybutton)
-        popup = Popup(title=title, content=box, size_hint=(None, None), size=(600, 300))
-        # mybutton.bind(on_release=self.stop)
-        popup.open()
+    #         grid_row.add_widget(leftImage)
+    #         grid_row.add_widget(leftLabel)
+    #         # grid_row.add_widget(favBtn)
 
 MainApp().run()
